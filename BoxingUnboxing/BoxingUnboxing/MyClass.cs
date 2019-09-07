99a43c8ef3509d0184ea58b98e386a72675473a9
Christopher Singleton <csnetsurfer@gmail.com>
2019-09-06 17:09:15 -0700

    Boxing and UnBoxing
    Understanding Variable Values into Objects and Object Values into Variables.

---
+++ BoxingUnboxing/BoxingUnboxing/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    /**************************** BOXING AND UNBOXING ********************************
+     *Boxing means when you pass information from a value type to a reference type.  *
+     *UnBoxing is when you pass a information from a reference type to a value type. *
+     *********************************************************************************
+    */
+    class MyClass
+    {
+        public void MyMethod()
+        {
+            // Boxing (Value Type to Reference Type). The value is passed from the variable, then to the object.
+            int Value = 1;
+            Object Obj = Value;
+
+            // UnBoxing (Reference Type to Value Type). The value is passed from the object to i.
+            int i = (int)Obj;
+
+            // Write out our value.
+            Console.WriteLine("The value that I have passed from Boxing to UnBoxing is: {0}", i);
+
+            // Pause the Console. 
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new object as "MyClass" using the new keyword and variable name to access the Class.
+            MyClass MyObjectName = new MyClass();
+
+            // Call "MyMethod" by including the Variable name of the Object.
+            MyObjectName.MyMethod();
+        }
+    }
+}
